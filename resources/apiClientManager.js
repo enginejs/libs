c69123de3d1d06c8aee27faec40b58cfdc727719
GyunerZeki <me@gzeki.com>
2016-10-06 22:12:53 +0100

    Add apiClientManager

---
+++ resources/apiClientManager.js
@@ -0,0 +1,45 @@
+var apiClientManager = {
+    baseUrl: "",
+    beforeSendCallback: function() {},
+
+    setBasicUrl: function(baseUrl) {
+        this.baseUrl = baseUrl;
+    },
+    setBeforeSendCallback: function(callback) {
+        this.beforeSendCallback = callback;
+    },
+    get: function(url, callback) {
+        this.request('GET', url, callback);
+    },
+    post: function(url, data, callback) {
+        this.request('POST', url, callback, data);
+    },
+    put: function(url, data, callback) {
+        this.request('PUT', url, callback, data);
+    },
+    delete: function(url, callback) {
+        this.request('DELETE', url, callback);
+    },
+    request: function(type, url, callback) {
+        var that = this;
+        $.ajax({
+            url: (this.baseUrl + url),
+            type: type,
+            success: function(result) {
+                callback.success(JSON.parse(result));
+            },
+            error: function(result) {
+                try {
+                    responseText = JSON.parse(result.responseText);
+                }
+                catch (e) {
+                    responseText = {};
+                }
+                callback.error(result.status, responseText);
+            },
+            beforeSend: function(xhr) {
+                that.beforeSendCallback(xhr);
+            },
+        });
+    }
+};
