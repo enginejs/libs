db81503d54ffe8d87df79bb0cdc512a4bdc9405c
GyunerZeki <me@gzeki.com>
2016-11-15 23:56:57 +0000

    refactor sendFormRequestManager

--- resources/sendFormRequestManager.js
+++ resources/sendFormRequestManager.js
@@ -1,84 +1,100 @@
-var sendFormRequestManager = {
-	init: function(formInstance, treatAllErrorAsGeneral) {
+var sendFormRequestManager = 
+{
+    formInstance: null,
+    treatAllErrorAsGeneral: true,
+    apiClient: null,
+    requestInProgress: false,
+    submitUrl: null,
 
-		var that = this;
-		// load empty event initially in case there are not set
-		that.subscribe('success', function(){});
-		that.subscribe('error', function(){});
-		that.subscribe('before', function(){ return true });
-		
-		if (typeof treatAllErrorAsGeneral == "undefined") {
-			var treatAllErrorAsGeneral = true;
-		}
-		
-		var errorBox     = formInstance.find('.form-error-box').first();
-		var submitButton = formInstance.find('input[type="submit"]').first();
-		
-		formInstance.submit(function(e) {
-			
-			e.preventDefault();
-			
-			var sentData = {};
-			formInstance.serializeArray().map(function(item){
-				sentData[item.name] = item.value;
-			});
+    onErrorCallback: function() {},
+    onGeneralErrorCallback: function() {},
+    onSuccessCallback: function() {},
+    beforeSubmitCallback: function() {},
 
-			// if ok, return true, if not return the error
-			executeBefore = that.trigger('before', {sentData: sentData});
+    init: function(newFormInstance, config) 
+    {
+        this.formInstance = $(newFormInstance);
 
-			if (executeBefore !== true) {
-				errorBox.removeClass('hidden').html(executeBefore);
-				return false;
-			}
-			
-			submitButton.prop("disabled", true);
-			errorBox.addClass('hidden');
-			
-			$.post(formInstance.attr('_action'), sentData)
-			.done(function(response) {
-				submitButton.prop("disabled", false);
-				
-				if (response['status'] == true) {
-					that.trigger('success', response['data']);
-				} else {
-					alert("Upss. somthing went wrong");
-				}
-			})
-			.error(function(data) {
-				var responseData = JSON.parse(data.responseText);
-				
-				if (typeof responseData["general"] != "undefined") {
-					errorBox.removeClass('hidden').html(responseData["general"]);
-				}
+        if (typeof config == "undefined") {
+            config = {};
+        }
 
-				if (treatAllErrorAsGeneral) {
-					var getFirstErrorField = function(errors)
-					{
-						for (var index in errors) {
-							var firstErorrField = errors[index];
-							
-							for (var index2 in firstErorrField) {
-								return firstErorrField[index2];
-							}
-						}
-					};
+        if (typeof config.treatAllErrorAsGeneral != "undefined") {
+            this.treatAllErrorAsGeneral = config.treatAllErrorAsGeneral;
+        }
 
-					errorBox.removeClass('hidden').html(getFirstErrorField(responseData));
-				}
-				
-				submitButton.prop("disabled", false);
-				that.trigger('error', responseData);
-			});
-		});	
+        if (typeof config.apiClient != "undefined") {
+            this.apiClient = config.apiClient;
+        }
 
-	},
-	subscribeList: {},
-	trigger: function(eventName, data) {
-		return this.subscribeList[eventName](data);
-	},
-	subscribe: function(eventName, callbackFunction){
-		return this.subscribeList[eventName] = callbackFunction;
-	},
+        if (typeof config.submitUrl != "undefined") {
+            this.submitUrl = config.submitUrl;
+        }
+
+        var that = this;
+
+        this.formInstance.on('submit', function(event) {
+            event.preventDefault();
+
+            if (that.requestInProgress == true) {
+                return ;
+            }
+            that.requestInProgress = true;
+
+            var sentData = {};
+            that.formInstance.serializeArray().map(function(item){
+                sentData[item.name] = item.value;
+            });
+
+            // that should be post not get request.
+            that.apiClient.get(submitUrl, {
+                success: function(response) {
+                    that.requestInProgress = false;
+                    // call propar erro based on status code
+                    that.onSuccessCallback(response);
+                },
+                error: function(status, response) {
+                    that.requestInProgress = false;
+
+                    if (status == 422) {
+                        if (that.treatAllErrorAsGeneral) {
+                            that.onGeneralErrorCallback(that.getFirstErrorField(response));
+                        } else {
+                            that.onErrorCallback(response);
+                        }
+                    } else if (status == 400) {
+                        that.onGeneralError(response);
+                    } else {
+                        console.log("NO idea about this error");
+                    }
+                },
+            });
+        });
+    },
+    getFirstErrorField: function(errors)
+    {
+        for (var index in errors) {
+            var firstErorrField = errors[index];
+            
+            for (var index2 in firstErorrField) {
+                return firstErorrField[index2];
+            }
+        }
+    },
+    onError: function(newOnError)
+    {
+        this.onErrorCallback = newOnError;
+    },
+    onGeneralError: function(newOnGeneralError)
+    {
+        this.onGeneralErrorCallback = newOnGeneralError;
+    },
+    onSuccess: function(newOnSuccess)
+    {
+        this.onSuccessCallback = newOnSuccess;
+    },
+    beforeSubmit: function(newBeforeSummit) 
+    {
+        this.beforeSubmitCallback = newBeforeSummit;
+    }
 };
-
-
