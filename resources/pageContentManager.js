3a159925d6f2c2d48188e35fdeee870d43507267
GyunerZeki <me@gzeki.com>
2017-03-05 11:23:21 +0000

    Pass config in pageContentManager when load the page.

--- resources/pageContentManager.js
+++ resources/pageContentManager.js
@@ -35,79 +35,67 @@ var pageContentManager = {
 
 		this.clientManager = options.clientManager;
 	},
-	load: function(url, onSuccessCallback) {
+	load: function(url, onSuccessCallback, viewFileDetail) {
 		
 		if (typeof onSuccessCallback == 'undefined') {
 			onSuccessCallback = function(){};
 		}
-		
+
 		that = this;
 		that.beforeLoad(url);
 
-		for (viewFilePattern in that.viewFilesPatterns) {
-			if (url.match(viewFilePattern)) {
+		var view = viewFileDetail['view'];
+		var partials = viewFileDetail['partials'];
+		var template = viewFileDetail['template'];
+		var loadData = viewFileDetail['loadData'];
 
-				var viewFileDetail = that.viewFilesPatterns[viewFilePattern];
-				
-				var view = viewFileDetail['view'];
-				var partials = viewFileDetail['partials'];
-				var template = viewFileDetail['template'];
-				var loadData = viewFileDetail['loadData'];
-				
-				loadPage = {
-					success: function(response) {
-						var pageContentInstance = that.changeTemplate(template);
+		loadPage = {
+			success: function(response) {
+				var pageContentInstance = that.changeTemplate(template);
 
-						// view
-						if (typeof partials != "undefined") {
-							// this should not work, it should actually load file!
-							for (partialName in partials) {
-								Handlebars.registerPartial(partialName, partials[partialName]);
-							}
-						}
-
-						var getView = function(viewName, onLoad)
-						{
-							// to keep it on localstorage in order to save network
-							$.get("/static/view/" + viewName + ".html", function(data) {
-								onLoad(data);
-							});
-							// catch error in case it not able to load it.
-						};
-
-						getView(view, function(viewContent){
-							// @todo catch errors here
-							var template = Handlebars.compile(viewContent);
-							pageContentInstance.html(template(response['data']));
-
-							that.afterAllSuccess();
-							onSuccessCallback(response['data']);
-							that.afterLoad();
-
-						});
-
-						// console.log("There is a no view pattern for this page.");
-						that.afterLoad();
-					},
-					error: function(result) {
-						// console.log('ERROR');
-						// console.log(result);
-						that.afterLoad();
-						that.onFail();
-					},
+				// view
+				if (typeof partials != "undefined") {
+					// this should not work, it should actually load file!
+					for (partialName in partials) {
+						Handlebars.registerPartial(partialName, partials[partialName]);
+					}
 				}
 
-				if (loadData == false) {
-					loadPage.success({'data' : null});
-				} else {
-					this.clientManager.get(that.urlPrefix + url, loadPage);
-				}
+				var getView = function(viewName, onLoad)
+				{
+					// to keep it on localstorage in order to save network
+					$.get("/static/view/" + viewName + ".html", function(data) {
+						onLoad(data);
+					});
+					// catch error in case it not able to load it.
+				};
 
-				break; // stop once it has a match.
-			}
+				getView(view, function(viewContent){
+					// @todo catch errors here
+					var template = Handlebars.compile(viewContent);
+					pageContentInstance.html(template(response['data']));
+
+					that.afterAllSuccess();
+					onSuccessCallback(response['data']);
+					that.afterLoad();
+
+				});
+
+				// console.log("There is a no view pattern for this page.");
+				that.afterLoad();
+			},
+			error: function(result) {
+				// console.log('ERROR');
+				// console.log(result);
+				that.afterLoad();
+				that.onFail();
+			},
 		}
-	},
-	setViewFilesPatterns: function(newViewFilesPatterns) {
-		this.viewFilesPatterns = newViewFilesPatterns;
+
+		if (loadData == false) {
+			loadPage.success({'data' : null});
+		} else {
+			this.clientManager.get(that.urlPrefix + url, loadPage);
+		}
 	}
 };
