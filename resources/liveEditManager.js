dad458217385f78e3fd21297165dc978fc47e598
GyunerZeki <me@gzeki.com>
2017-07-05 18:54:00 +0100

    Finish liveEditManager

--- resources/liveEditManager.js
+++ resources/liveEditManager.js
@@ -1,76 +1,83 @@
-var EngineJS_liveEditManager = 
+var EngineJS_liveEditManager =
 {
-        apiClient: null,
+    apiClient: null,
+    submitUrl: "",
 
-        // events.
-        before: function() {},
-        onError: function() {},
-        onSuccess: function() {},
+    // events.
+    before: function() {},
+    onError: function() {},
+    onSuccess: function() {},
 
-        init: function(instance, options) {
-            this.url = options.url;
-            this.name = options.name;
-            this.apiClient = options.apiClient;
+    init: function(instance, options) {
+        this.submitUrl = options.submitUrl;
+        this.name = options.name;
+        this.apiClient = options.apiClient;
 
-            var instance = $(instance);
-            var type = instance.prop('tagName');
-            var timeoutId;
-            var that = this;
+        var instance = $(instance);
+        var type = instance.prop('tagName');
+        var timeoutId;
+        var that = this;
 
-            var saveChanges = function(newValue){
-                that.before(newValue, that.name);
+        var saveChanges = function(newValue){
+            that.before(newValue, that.name);
 
-                // some ajax reqeust here.
-                that.onError(newValue, that.name);
-                that.onSuccess(newValue, that.name);
-            };
+            var sendData = {};
 
-            if (type == 'INPUT') {
-                var exactType = $(instance).attr('type');
-                
-                if (exactType == 'text') {
-                    instance.keypress(function () {
-                        if (timeoutId) clearTimeout(timeoutId);
-                            timeoutId = setTimeout(function () {
-                            saveChanges(instance.val());
-                        }, 650);
-                    });
-                } else if (exactType == 'checkbox') {
-                    instance.change(function(){
-                        saveChanges($(this).is(':checked'));
-                    });
-                } else if (exactType == 'radio') {
-                    instance.change(function(){
-                        saveChanges($(this).val());
-                    });
-                } else {
-                    console.log('Not supported input for autosave.');
+            that.apiClient.put(that.submitUrl, sendData, {
+                success: function() {
+                    that.onSuccess(newValue, that.name);
+                }, 
+                error: function() {
+                    that.onError(newValue, that.name);
                 }
-            } else if (type == 'TEXTAREA') {
+            });
+        };
 
+        if (type == 'INPUT') {
+            var exactType = $(instance).attr('type');
+            
+            if (exactType == 'text') {
                 instance.keypress(function () {
                     if (timeoutId) clearTimeout(timeoutId);
                         timeoutId = setTimeout(function () {
                         saveChanges(instance.val());
                     }, 650);
                 });
-
-            } else if (type == 'SELECT') {
+            } else if (exactType == 'checkbox') {
                 instance.change(function(){
-                    saveChanges(instance.val());
+                    saveChanges($(this).is(':checked'));
+                });
+            } else if (exactType == 'radio') {
+                instance.change(function(){
+                    saveChanges($(this).val());
                 });
             } else {
                 console.log('Not supported input for autosave.');
             }
+        } else if (type == 'TEXTAREA') {
 
-        },
-        before: function(before) {
-            this.before = before
-        },
-        onError: function(onError) {
-            this.onError = onError
-        },
-        onSuccess: function(onSuccess) {
-            this.onSuccess = onSuccess
-        },
-    };
+            instance.keypress(function () {
+                if (timeoutId) clearTimeout(timeoutId);
+                    timeoutId = setTimeout(function () {
+                    saveChanges(instance.val());
+                }, 650);
+            });
+
+        } else if (type == 'SELECT') {
+            instance.change(function(){
+                saveChanges(instance.val());
+            });
+        } else {
+            console.log('Not supported input for autosave.');
+        }
+    },
+    before: function(before) {
+        this.before = before
+    },
+    onError: function(onError) {
+        this.onError = onError
+    },
+    onSuccess: function(onSuccess) {
+        this.onSuccess = onSuccess
+    },
+};
